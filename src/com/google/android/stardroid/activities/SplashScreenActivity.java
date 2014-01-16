f981c9fa687f2b3fd80feef24447746d6db26761
Paul Peavyhouse <pv@twistpair.com>
2014-01-16 15:40:28 -0800

    Reverting formatting differences from upstream

--- src/com/google/android/stardroid/activities/SplashScreenActivity.java
+++ src/com/google/android/stardroid/activities/SplashScreenActivity.java
@@ -32,61 +32,58 @@ import com.google.android.stardroid.util.OsVersions;
  * Shows a splash screen, then launch the next activity.
  */
 public class SplashScreenActivity extends Activity {
-	private final static String TAG = MiscUtil
-			.getTag(SplashScreenActivity.class);
+  private final static String TAG = MiscUtil.getTag(SplashScreenActivity.class);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		Log.d(TAG, "SplashScreen onCreate");
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.splash);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    Log.d(TAG, "SplashScreen onCreate");
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.splash);
 
-		final View graphic = findViewById(R.id.splash);
-		OsVersions.setSystemStatusBarVisible(findViewById(R.id.splash), false);
+    final View graphic = findViewById(R.id.splash);
+    OsVersions.setSystemStatusBarVisible(findViewById(R.id.splash), false);
 
-		Animation fadeAnimation = AnimationUtils.loadAnimation(this,
-				R.anim.fadeout);
-		fadeAnimation.setAnimationListener(new AnimationListener() {
-			public void onAnimationEnd(Animation arg0) {
-				Log.d(TAG, "SplashScreen.Animation onAnimationEnd");
-				graphic.setVisibility(View.INVISIBLE);
-				finish();
-			}
+    Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+    fadeAnimation.setAnimationListener(new AnimationListener() {
+      public void onAnimationEnd(Animation arg0) {
+        Log.d(TAG, "SplashScreen.Animation onAnimationEnd");
+        graphic.setVisibility(View.INVISIBLE);
+        finish();
+      }
 
-			public void onAnimationRepeat(Animation arg0) {
-			}
+      public void onAnimationRepeat(Animation arg0) {
+      }
 
-			public void onAnimationStart(Animation arg0) {
-				Log.d(TAG, "SplashScreen.Animcation onAnimationStart");
-			}
-		});
-		graphic.startAnimation(fadeAnimation);
-	}
+      public void onAnimationStart(Animation arg0) {
+        Log.d(TAG, "SplashScreen.Animcation onAnimationStart");
+      }
+    });
+    graphic.startAnimation(fadeAnimation);
+  }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		Analytics.getInstance(this).trackPageView(
-				Analytics.SPLASH_SCREEN_ACTIVITY);
-	}
+  @Override
+  public void onStart() {
+    super.onStart();
+    Analytics.getInstance(this).trackPageView(Analytics.SPLASH_SCREEN_ACTIVITY);
+  }
 
-	@Override
-	public void onPause() {
-		Log.d(TAG, "SplashScreen onPause");
-		super.onPause();
-	}
+  @Override
+  public void onPause() {
+    Log.d(TAG, "SplashScreen onPause");
+    super.onPause();
+  }
 
-	@Override
-	public void onDestroy() {
-		Log.d(TAG, "SplashScreen onDestroy");
-		super.onDestroy();
-	}
+  @Override
+  public void onDestroy() {
+    Log.d(TAG, "SplashScreen onDestroy");
+    super.onDestroy();
+  }
 
-	@Override
-	public void finish() {
-		Log.d(TAG, "SplashScreen finish called");
-		Intent intent = new Intent(this, DynamicStarMapActivity.class);
-		startActivity(intent);
-		super.finish();
-	}
+  @Override
+  public void finish() {
+    Log.d(TAG, "SplashScreen finish called");
+    Intent intent = new Intent(this, DynamicStarMapActivity.class);
+    startActivity(intent);
+    super.finish();
+  }
 }
--- src/com/google/android/stardroid/activities/TosActivity.java
+++ src/com/google/android/stardroid/activities/TosActivity.java
@@ -7,31 +7,31 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
-
 import com.google.android.stardroid.R;
 
-public class TosActivity extends Activity {
+public class tosActivity extends Activity {
+	
+	    
+	  @Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.tos_view);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.tos_view);
-
+	  }
+	  
+	  public void agreeClick(View view)  {  
+		  SharedPreferences sharedPreferences =
+                  PreferenceManager.getDefaultSharedPreferences(this);
+              Editor editor = sharedPreferences.edit();
+              editor.putBoolean(DynamicStarMapActivity.READ_TOS_PREF, true);
+              editor.commit();
+              Intent intent = new Intent(this, DynamicStarMapActivity.class);
+              startActivity(intent);
+              super.finish();
+	  }  
+	  
+	  public void disagreeClick(View view)  {  
+		  super.finish();
+	  }  
+	 
 	}
-
-	public void agreeClick(View view) {
-		SharedPreferences sharedPreferences = PreferenceManager
-				.getDefaultSharedPreferences(this);
-		Editor editor = sharedPreferences.edit();
-		editor.putBoolean(DynamicStarMapActivity.READ_TOS_PREF, true);
-		editor.commit();
-		Intent intent = new Intent(this, DynamicStarMapActivity.class);
-		startActivity(intent);
-		super.finish();
-	}
-
-	public void disagreeClick(View view) {
-		super.finish();
-	}
-
-}
